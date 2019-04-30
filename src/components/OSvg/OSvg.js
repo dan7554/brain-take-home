09d9fbe936da2bbed760d89f1ce6a51dbe5e7ab8
Daniel Christiani <>
2019-04-30 17:50:57 -0600

    Added most of the game logic

--- src/components/OSvg/OSvg.js
+++ src/components/OSvg/OSvg.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const OSVG = ({className}) => (
-    <div className={`o-svg ${className}`}>
+const OSVG = ({className, style}) => (
+    <div style={style} className={`o-svg ${className}`}>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="-3 -3 30 30"><path d="M0 0h24v24H0z" fill="none" /><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" /></svg>
     </div>
 
--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -8,21 +8,50 @@ class TicTacToe extends React.Component {
     state = {
         pieceOne: null,
         pieceTwo: null,
-        step: 0
+        step: 0,
+        game: [],
+        turn: 'x'
     }
 
     updateStep = step => () => {
         switch (step) {
-            case 'pick':
+            case 'mode':
                 this.setState({ step: 0 });
                 break;
-            case 'game':
+            case 'pick':
                 this.setState({ step: 1 });
+                break;
+            case 'game':
+                this.setState({ step: 2 });
                 break;
             default:
                 this.setState({ step: 0 });
                 break;
         }
+    }
+
+    onSpotClick = spot => () => {
+        const { pieceOne, pieceTwo, game, turn } = this.state
+
+        if (!game[spot]) {
+            const imageElement = new Image()
+            const xPiece = pieceOne || '/assets/images/X.svg'
+            const oPiece = pieceTwo || '/assets/images/O.svg'
+            imageElement.src = turn === 'x' ? xPiece : oPiece
+            imageElement.className = 'game__board-spot-image'
+            imageElement.onload = () => {
+                const spotContainer = window.currentDocument.querySelector(`#spot-${spot}`)
+                if (spotContainer) {
+                    spotContainer.appendChild(imageElement)
+                }
+            }
+            game[spot] = turn
+            this.setState({ game, turn: turn === 'x' ? 'o' : 'x' })
+        }
+    }
+
+    onModeClick = mode => () => {
+        this.setState({ mode })
     }
 
     inputFileUpload = piece => fileEvent => {
@@ -44,20 +73,52 @@ class TicTacToe extends React.Component {
         this.setState({ selected: piece })
     }
 
+    renderMode() {
+        const { mode } = this.state
+        const selectedClass = selected => selected === mode ? 'selected' : ''
+
+        return (
+            <div className="mode">
+                <div className="mode__title">
+                    Pick your mode!
+            </div>
+                <div className="mode__container">
+                    <div className={`mode__button ${selectedClass('single')}`}onClick={this.onModeClick('single')}>
+                        Singleplayer
+                </div>
+                    <div className={`mode__button ${selectedClass('multi')}`} onClick={this.onModeClick('multi')}>
+                        Multiplayer
+                </div>
+                </div>
+                <div className="mode__next-container">
+                    {mode && <div className="mode__next" onClick={this.updateStep('pick')}>
+                        Next
+                    </div>}
+                </div>
+            </div>
+        )
+    }
+
     renderPick() {
-        const { pieceOne, pieceTwo, selected } = this.state
-        const selectedClass = piece => selected === piece ? 'selected' : ''
+        const { pieceOne, pieceTwo, selected, mode } = this.state
+        const isMultiplayer = mode === 'multi'
+        const isReady = selected || isMultiplayer
+        const selectedClass = piece => selected === piece && !isMultiplayer ? 'selected' : ''
+        const pieceStyle = { border: isMultiplayer ? 'none' : '', cursor: isMultiplayer ? 'initial' : '' }
 
         return (
             <div className="pick-pieces">
                 <div className="pick-pieces__title">
-                    Pick your piece!
+                    {isMultiplayer ? "Upload your pieces!" : "Pick your piece!"}
                 </div>
                 <div className="pick-pieces__container">
                     <div className="pick-pieces__piece-one">
+                        <div className="pick-pieces__player">
+                        {isMultiplayer ? 'Player 1' : selected ? selected === 'pieceOne' ? 'Player 1' : 'Computer' : ''}
+                        </div>
                         <div onClick={this.selectPiece('pieceOne')}>
-                            {pieceOne && <img src={pieceOne} className={selectedClass('pieceOne')} />}
-                            {!pieceOne && <XSvg className={selectedClass('pieceOne')} />}
+                            {pieceOne && <img style={pieceStyle} src={pieceOne} className={selectedClass('pieceOne')} />}
+                            {!pieceOne && <XSvg style={pieceStyle} className={selectedClass('pieceOne')} />}
                         </div>
                         <div className="pick-pieces__input-one">
                             <input
@@ -70,9 +131,12 @@ class TicTacToe extends React.Component {
                         </div>
                     </div>
                     <div className="pick-pieces__piece-two">
+                        <div className="pick-pieces__player">
+                            {isMultiplayer ? 'Player 2' : selected ? selected === 'pieceTwo' ? 'Player 1' : 'Computer' : '' }
+                        </div>
                         <div onClick={this.selectPiece('pieceTwo')}>
-                            {pieceTwo && <img src={pieceTwo} className={selectedClass('pieceTwo')} />}
-                            {!pieceTwo && <OSvg className={selectedClass('pieceTwo')} />}
+                            {pieceTwo && <img style={pieceStyle} src={pieceTwo} className={selectedClass('pieceTwo')} />}
+                            {!pieceTwo && <OSvg style={pieceStyle} className={selectedClass('pieceTwo')} />}
                         </div>
                         <div className="pick-pieces__input-two">
                             <input
@@ -86,7 +150,7 @@ class TicTacToe extends React.Component {
                     </div>
                 </div>
                 <div className="pick-pieces__play-container">
-                    {selected && <div className="pick-pieces__play" onClick={this.updateStep('game')}>
+                    {isReady && <div className="pick-pieces__play" onClick={this.updateStep('game')}>
                         Play
                     </div>}
                 </div>
@@ -97,7 +161,20 @@ class TicTacToe extends React.Component {
     renderGame() {
         return (
             <div className="game">
-                game
+                <div className="game__board">
+                    <div id="spot-0" className="game__board-spot tl" onClick={this.onSpotClick(0)} />
+                    <div id="spot-1" className="game__board-spot tm" onClick={this.onSpotClick(1)} />
+                    <div id="spot-2" className="game__board-spot tr" onClick={this.onSpotClick(2)} />
+
+                    <div id="spot-3" className="game__board-spot ml" onClick={this.onSpotClick(3)} />
+                    <div id="spot-4" className="game__board-spot mm" onClick={this.onSpotClick(4)} />
+                    <div id="spot-5" className="game__board-spot mr" onClick={this.onSpotClick(5)} />
+
+                    <div id="spot-6" className="game__board-spot bl" onClick={this.onSpotClick(6)} />
+                    <div id="spot-7" className="game__board-spot bm" onClick={this.onSpotClick(7)} />
+                    <div id="spot-8" className="game__board-spot br" onClick={this.onSpotClick(8)} />
+
+                </div>
             </div>
         )
     }
@@ -108,6 +185,9 @@ class TicTacToe extends React.Component {
 
         return (
             <div className={`tic-tac-toe ${breakpoint}`}>
+                <div className="tic-tac-toe__mode" style={{ transform: `translateX(${-100 * step}%)` }}>
+                    {this.renderMode()}
+                </div>
                 <div className="tic-tac-toe__pick" style={{ transform: `translateX(${-100 * step}%)` }}>
                     {this.renderPick()}
                 </div>
